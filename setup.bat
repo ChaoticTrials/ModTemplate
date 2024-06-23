d787a1101f4856de0e9452fdcec6eab63bf837c5
MelanX <info@melanx.de>
2024-06-23 23:05:47 +0200

    update neoforge

--- setup.bat
+++ setup.bat
@@ -174,7 +174,7 @@ echo parchment_mappings_version=2024.06.02
 echo.
 echo ## Loader Properties
 echo minecraft_version=1.21
-echo neo_version=21.0.0-beta
+echo neo_version=21.0.30-beta
 echo loader_version_range=[4,^)
 echo.
 echo ## Mod Properties
